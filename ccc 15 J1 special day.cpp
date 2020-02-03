51c919e9bb92f27bbdbe94689b45e278866fa3b2
dddf <52809502+haonanq666@users.noreply.github.com>
2020-02-03 01:28:50 -0500

    Create ccc 15 J1 special day.cpp

---
+++ ccc 15 J1 special day.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int month, day;
+    cin>>month>>day;
+    
+    if(month<2){
+        cout << "Before"<<endl;
+    }else if(month > 2){
+        cout << "After"<<endl;
+        
+    }else if(month ==2){
+        if(day<18){
+            cout<<"Before"<<endl;
+        }else if (day >18){
+            cout << "After" <<endl;
+            
+        }else if(day ==18){
+            cout << "Special"<< endl;
+        }else{
+            cout << "online judge broken this should not happen"<<endl;
+        }
+    }
+    return 0;
+}
